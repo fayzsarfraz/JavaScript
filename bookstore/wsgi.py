7d45715d9a2731cef1aa581a27ab0d8a30b07f91
Fayz Sarfraz <125624754+fayzsarfraz@users.noreply.github.com>
2024-01-29 21:34:44 +0500

    asdfasdfasdf

--- bookstore/wsgi.py
+++ bookstore/wsgi.py
@@ -1,17 +1,33 @@
 """
-WSGI config for bookstore project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
+WSGI config for vercel_app project.
+It exposes the WSGI callable as a module-level variable named ``app``.
 For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bookstore.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vercel_app.settings')
 
-application = get_wsgi_application()
-app = application
+
+app = get_wsgi_application()
+
+# """
+# WSGI config for bookstore project.
+
+# It exposes the WSGI callable as a module-level variable named ``application``.
+
+# For more information on this file, see
+# https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+# """
+
+# import os
+
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bookstore.settings')
+
+# application = get_wsgi_application()
+# app = application
